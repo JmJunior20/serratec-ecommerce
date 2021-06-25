8b7eff35680d7d9a84b9ad4016f057b8861903d7
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-25 05:37:19 -0300

    Criando a logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,14 @@ import { NavBar } from "../../styles/header-styles";
 import { InputTextDois } from "../../styles/forms-styles";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 
 const Menu = () => {
 
     return (
         <NavBar>
             <a>
-                <Link to={'/'}><h1 color="red">SerraBook</h1></Link>
+                <Link to={'/'}><h1 color="red"><ImportContactsIcon /> SerraBook</h1></Link>
             </a>
                 
                 <InputTextDois 
